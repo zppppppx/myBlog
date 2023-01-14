db08734def32a944b665ac3d4e52c5a86fbcd41b
zppppppx <zppppppx@gmail.com>
2023-01-14 09:31:36 -0800

    1. Added the user model

---
+++ src/server/model/user.cjs
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    url_cover: {
+        type: String,
+        default: '',
+    },
+    // self-introduce
+    introduction: {
+        type: String,
+        default: '',
+    },
+    // avatar
+    avatar: {
+        type: String,
+        default: '',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
