e6639a1f052b5625af917043477edb5e29c1ed03
zppppppx <zppppppx@gmail.com>
2023-01-14 09:27:30 -0800

    1. Added a simple footer to get started;

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  
-  <router-view/>
-
+  <VApp>
+    <VMain>
+      
+      <router-view />
+      <footerVue />
+    </VMain>
+  </VApp>
 </template>
+
+<script>
+import footerVue from './components/footerVue.vue';
+export default {
+  components: {
+    footerVue
+  }
+}
+</script>
+
 
 <style scoped>
 .logo {
@@ -17,5 +31,16 @@
 
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: -8px -8px auto;
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/server/app.cjs
+++ src/server/app.cjs
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 
+// What we need for the CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
