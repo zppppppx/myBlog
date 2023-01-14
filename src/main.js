404be13d3c4a9a1219ecf9eaff4166e62d078584
zppppppx <zppppppx@gmail.com>
2023-01-14 09:28:43 -0800

    1. Tried to use element-plus but fount out vuetify more satisfying

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import vuetify from './plugin/vuetify'
+import ElementPlus from './plugin/elementPlus'
 
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-createApp(App).use(router).use(vuetify).mount('#app')
+// import 'vuetify/styles'
+// import { createVuetify } from 'vuetify'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+
+// const vuetify =  createVuetify({
+//     components,
+//     directives,
+// })
+
+createApp(App).use(router).use(vuetify).use(ElementPlus).mount('#app')
