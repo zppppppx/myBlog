3b5404db64f63b7bb81062e8bb9b51c4d44b2441
zppppppx <zppppppx@gmail.com>
2023-01-18 13:52:09 -0800

    1. Modified the footer to a better view.

--- src/components/footerVue.vue
+++ src/components/footerVue.vue
@@ -1,27 +1,51 @@
+
+
 <template>
-  <v-footer class="bg-grey-lighten-1">
-    <v-row justify="center" no-gutters>
-      <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
-        {{ link }}
-      </v-btn>
-      <v-col class="text-center mt-4" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
+
+  <div class="footer">
+    <span class="content">jokes</span>
+    <span class="cpyright">copy right</span>
+  </div>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    links: [
-      'Home',
-      'About Us',
-      'Team',
-      'Services',
-      'Blog',
-      'Contact Us',
-    ],
-  }),
-}
+
 </script>
+
+<style scoped lang="less">
+.footer {
+  width: 100%;
+  height: 70px;
+  border-radius: 25px 25px 0 0;
+  background: linear-gradient(-45deg, #79CBFF, #4EDEC4,
+  #9BE065, #FAF370);
+   background-size: 300% 300%;
+  margin-top: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  animation: gradientBG 5s ease infinite;
+
+  .content {
+    margin: 10px 0px 6px 0px;
+    font-size: 18px;
+  }
+
+  .cpyright {
+    color: gray;
+  }
+}
+
+@keyframes gradientBG {
+  0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+</style>
