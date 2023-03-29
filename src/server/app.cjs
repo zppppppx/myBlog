65140b395bd7e0dc90b52c65d81f94f73ea52a44
zppppppx <zppppppx@gmail.com>
2023-03-29 00:48:57 -0700

    Modified some of the authentication settings

--- src/server/app.cjs
+++ src/server/app.cjs
@@ -5,10 +5,13 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
 
 const app = express()
-const { dbUrl } = require('./Config.cjs');
-
+const { dbUrl, sessionConfig } = require('./Config.cjs');
+const User = require('./model/user.cjs')
 
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
@@ -17,18 +20,37 @@ db.once('open', () => {
     console.log('Database connected', dbUrl);
 });
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
 
+// app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+// app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json({ limit: '50mb' }));
 
 // What we need for the CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Origin', '*') // TODO: set this to the specific name
+    res.header('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, transformRequest")
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS,HEAD')
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header('Access-Control-Request-Headers', 'transformrequest')
     res.header('X-Powered-By', ' 3.2.1')
     req.method == "OPTIONS" ? res.sendStatus(200) : next()
 })
+
+
+
+app.use(session(sessionConfig))
+
+app.use(passport.initialize()); // To initialize the passport
+app.use(passport.session()); // To let express session work, must be put after express session
+passport.use(new LocalStrategy(User.authenticate()));
+// passport.use(new LocalStrategy(User.createStrategy()));
+
+passport.serializeUser(User.serializeUser()); // How to store user in the session
+passport.deserializeUser(User.deserializeUser()); // How to get user out of the session
+
+
+
 
 
 const adminRouter = require('./routers/admin.cjs');
